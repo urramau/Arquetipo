9e4e35508d2bd75c2c103ecde0b5e02ec3d22035
Urrex <npmauricio@gmail.com>
2020-10-19 09:15:09 -0300

    Agregar archivos de proyecto.

---
+++ Arquetipo.Infrastructure/Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Arquetipo.Infrastructure.Data
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        { }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
+                .HasAnnotation("Relational:DefaultSchema", "UPRESTA");
+        }
+    }
+}
---
+++ Arquetipo.Infrastructure/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Prestamo.Simulacion.Core.Exceptions;
+using System.Net;
+
+namespace Arquetipo.Infrastructure.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception.GetType() == typeof(BusinessException))
+            {
+                var exception = (BusinessException)context.Exception;
+                var validation = new
+                {
+                    Status = 400,
+                    Title = "Bad Request",
+                    Resultado = false,
+                    Datos = "",
+                    Errores = exception.Message
+                };
+
+                context.Result = new BadRequestObjectResult(validation);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.ExceptionHandled = true;
+            }
+        }
+
+    }
+}
